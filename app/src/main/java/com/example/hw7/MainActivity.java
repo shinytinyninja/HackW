cc72a20f83d7f73a6b287fb1e7803d8fa1c963fc
Jeffrey Wong <jeffreywong155@gmail.com>
2020-11-21 12:48:44 -0500

    Initial commit

---
+++ app/src/main/java/com/example/hw7/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.hw7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_main);
+    }
+}
